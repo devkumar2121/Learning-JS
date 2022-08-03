b02403695ca9d848f0d59be690123f7c0dba5d35
Dev Kumar <dev.kumar@sourcesoftsolutions.com>
2022-08-03 20:51:43 +0530

    push

---
+++ index.js
@@ -0,0 +1,19 @@
+/* conditional statement are used to perform differnent actions based on differnent conditions
+var marks = 100;
+if (marks == 10) {
+  console.log("Yes your marks is correct");
+} else {
+  console.log("No your marks is incorrect");
+}
+ */
+
+/* comparison operator
+ */
+// var a = 1;
+// var b = '1';
+// if(a === b){
+//     console.log("both are equal")
+// }else{
+//     console.log("Both are not equal")
+// }
+
