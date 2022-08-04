9c7c70810fc92d822c2cbe1702f2549932259cbb
devkumar2121 <devkashyap2121@gmail.com>
2022-08-04 10:27:51 +0530

    push

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 /* conditional statement are used to perform differnent actions based on differnent conditions
 var marks = 100;
 if (marks == 10) {
@@ -7,13 +9,47 @@ if (marks == 10) {
 }
  */
 
-/* comparison operator
- */
-// var a = 1;
-// var b = '1';
-// if(a === b){
-//     console.log("both are equal")
-// }else{
-//     console.log("Both are not equal")
-// }
 
+/* comparison operator
+== is equal to
+=== is identical(its equal to and is of the same type)
+!= is not equal to
+!== is not identical
+> greater than
+>= greater than or equal to
+< less than
+<= less than or equal to
+
+--> Assignment operator
+= assign
++= add and assign
+-= subtract and assign
+*= multiply and assign
+/= divide and assign
+%= modulus and assign
+
+var a = 1;
+var b = '1';
+if(a === b){
+    console.log("both are equal")
+}else{
+    console.log("Both are not equal")
+}
+ */
+
+/*
+switch statement - statement is used to select one of many blocks of code
+to be executed
+*/
+
+/*
+Iterative statement
+Iteration literally means repetition of process. So iterative statements are constructs that help us repeat some statements a defined
+no of times. The different iterative statement available in JS are
+1-do while loop
+2-while loop
+3-for loop
+
+loop - loops can execute a block of code as long as a specified condition is true
+while loop - while loop through a block of code as long as a specified condition is true
+ */
